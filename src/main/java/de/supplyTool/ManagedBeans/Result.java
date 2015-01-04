eb3acb8d2853713ada0959197c918cd8631e7edb
Vassil Dimov <vsldimov@gmail.com>
2015-01-05 00:30:11 +0100

    Download functionality added. It has to be connected with the xml output in the console

--- src/main/java/de/supplyTool/ManagedBeans/Result.java
+++ src/main/java/de/supplyTool/ManagedBeans/Result.java
@@ -1,12 +1,19 @@
 package de.supplyTool.ManagedBeans;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
 
 import de.supplyTool.ManagedBeans.disposition.KaufTeilDispositionErgebnis;
 import de.supplyTool.ManagedBeans.disposition.Produktionsprogramm;
+import de.supplyTool.ManagedBeans.prodVerwaltung.ProdVerwaltungBean;
 import de.supplyTool.dao.Dao;
 import de.supplyTool.domain.APArbeitsplatz;
 import de.supplyTool.domain.APTeil;
@@ -129,6 +136,44 @@ public class Result {
         result = sb.toString();
         System.out.println("result " + result);
 
+    }
+    
+    public void downloadFile() {
+    	
+    	final String path = ProdVerwaltungBean.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+
+        // Klassenspezifische sachen abschneiden
+        String destinationDir = path.replace("ManagedBeans/prodVerwaltung/ProdVerwaltungBean.class", "");
+        
+        destinationDir = destinationDir + "xmlFiles" + "/results.xsd";
+
+        File file = new File(destinationDir);
+        HttpServletResponse response = (HttpServletResponse) FacesContext.getCurrentInstance().getExternalContext().getResponse();  
+
+        response.setHeader("Content-Disposition", "attachment;filename=file.txt");  
+        response.setContentLength((int) file.length());  
+        ServletOutputStream out = null;  
+        try {  
+            FileInputStream input = new FileInputStream(file);  
+            byte[] buffer = new byte[1024];  
+            out = response.getOutputStream();  
+            int i = 0;  
+            while ((i = input.read(buffer)) != -1) {  
+                out.write(buffer);  
+                out.flush();  
+            }  
+            FacesContext.getCurrentInstance().getResponseComplete();  
+        } catch (IOException err) {  
+            err.printStackTrace();  
+        } finally {  
+            try {  
+                if (out != null) {  
+                    out.close();  
+                }  
+            } catch (IOException err) {  
+                err.printStackTrace();  
+            }  
+        }  
     }
 
     public String getResult() {
