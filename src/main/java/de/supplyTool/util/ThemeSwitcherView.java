43a0120d14cd4961b5037074a8797de0523dd8d8
pefi1011 <pefi1011@gmail.com>
2015-01-06 00:06:48 +0100

    mainForm zugefuegt und ThemeSwitcher - funktioniert noch nicht

---
+++ src/main/java/de/supplyTool/util/ThemeSwitcherView.java
@@ -0,0 +1,31 @@
+package de.supplyTool.util;
+
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.RequestScoped;
+ 
+@ManagedBean
+@RequestScoped
+public class ThemeSwitcherView {
+ 
+    private List<Theme> themes;
+     
+    @ManagedProperty("#{themeService}")
+    private ThemeService service;
+ 
+    @PostConstruct
+    public void init() {
+        themes = service.getThemes();
+    }
+     
+    public List<Theme> getThemes() {
+        return themes;
+    } 
+ 
+    public void setService(ThemeService service) {
+        this.service = service;
+    }
+}
